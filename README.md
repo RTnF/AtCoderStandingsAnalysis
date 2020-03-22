a471de93a0d1e6e047ab6774d923b2e21070837f
RTnF <50510242+RTnF@users.noreply.github.com>
2020-03-22 19:30:43 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AtCoderStandingsAnalysis
+
+順位表のjsonを集計し、上部にテーブルを追加します。  
---
+++ main.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         AtCoderStandingsAnalysis
+// @namespace    https://github.com/RTnF/AtCoderStandingsAnalysis
+// @version      0.0.0
+// @description  順位表のjsonを集計し、上部にテーブルを追加します。
+// @author       RTnF
+// @match        https://atcoder.jp/*standings*
+// @exclude      https://atcoder.jp/*standings/json
+// @grant        none
+// ==/UserScript==
+
+// ソート済み配列のうちval未満が何個あるか求める
+function countLower(arr, val) {
+  var lo = -1;
+  var hi = arr.length;
+  while (hi - lo > 1) {
+    var mid = Math.floor((hi + lo) / 2);
+    if (arr[mid] < val) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  }
+  return hi;
+}
+
+// 換算: Rating -> innerRating
+function innerRating(rate) {
+  if (rate < 400) {
+    rate = 400 * (1 - Math.log(400 / rate));
+  }
+  rate += 1200 * (Math.sqrt(1 - Math.pow(0.81, data[j].Competitions)) / (1 - Math.pow(0.9, data[j].Competitions)) - 1) / (Math.sqrt(19) - 1);
+  return rate;
+}
+
+$(function () {
+  'use strict';
+
+  // 表のヘッダーを先頭に追加する
+  $('#vue-standings').prepend(`
+<div>
+  <table id="acsa-table" class="table table-bordered table-hover th-center td-center td-middle">
+    <thead>
+    <tr>
+      <th>問題</th>
+      <th>得点</th>
+      <th>人数</th>
+      <th>正解率</th>
+      <th>平均ペナ</th>
+      <th>レート</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</div>
+  `);
+
+  // 表の更新
+  setInterval(function () {
+    var data = vueStandings.standings.StandingsData;
+    var task = vueStandings.standings.TaskInfo;
+    const cols = ["#808080", "#804000", "#008000", "#00C0C0", "#0000FF", "#C0C000", "#FF8000", "#FF0000"];
+    const threshold = [-10000, 400, 800, 1200, 1600, 2000, 2400, 2800];
+    const canvasWidth = 250;
+
+    $('#acsa-table > tbody').empty();
+    for (let i = 0; i < task.length; i++) {
+      var isTried = vueStandings.tries[i] > 0;
+      $('#acsa-table > tbody').append(`
+<tr>
+  <td>` + task[i].Assignment + `</td>
+  <td></td>
+  <td>` + vueStandings.ac[i] + ` / ` + vueStandings.tries[i] + `</td>
+  <td>` + (isTried ? (vueStandings.ac[i] / vueStandings.tries[i] * 100).toFixed(2) + "%" : "-") + `</td>
+  <td></td>
+  <td><canvas width="` + canvasWidth + `px" height="20px"></canvas></td>
+</tr>
+      `);
+
+      // トップの得点を満点とみなす
+      var maxScore = -1;
+      var myScore = -1;
+      // 提出者数 / ペナルティ数
+      var avePenalty = 0;
+      var rates = [];
+      for (let j = 0; j < data.length; j++) {
+        var result = data[j].TaskResults[task[i].TaskScreenName];
+        // 未提出のときresult === undefined
+        if (result) {
+          if (data[j].UserScreenName === vueStandings.userScreenName) {
+            myScore = result.Score;
+          }
+          if (result.Score === 0) {
+            avePenalty += result.Failure;
+          } else {
+            avePenalty += result.Penalty;
+          }
+          if (maxScore < result.Score) {
+            maxScore = result.Score;
+          }
+        }
+      }
+      
+      // 正解者の内部レート配列を作成する
+      for (let j = 0; j < data.length; j++) {
+        if (data[j].Competitions > 0
+        &&  data[j].TaskResults[task[i].TaskScreenName]
+        &&  data[j].TaskResults[task[i].TaskScreenName].Score === maxScore) {
+          rates.push(innerRating(data[j].Rating));
+        }
+      }
+      rates.sort(function (a, b) { return a - b; });
+      
+      myScore /= 100;
+      maxScore /= 100;
+      if (isTried) {
+        avePenalty /= vueStandings.tries[i];
+      }
+
+      $('#acsa-table > tbody > tr:eq(' + i + ') > td:eq(1)').text(myScore >= 0 ? myScore.toFixed() : "-");
+      $('#acsa-table > tbody > tr:eq(' + i + ') > td:eq(4)').text(isTried ? avePenalty.toFixed(2) : "-");
+      if (isTried) {
+        var canvas = $('#acsa-table > tbody > tr:eq(' + i + ') > td:eq(5) > canvas')[0];
+        if (canvas.getContext) {
+          var context = canvas.getContext('2d');
+          for (let k = 0; k < 8; k++) {
+            context.fillStyle = cols[k];
+            var x = Math.round(countLower(rates, threshold[k]) / rates.length * canvasWidth);
+            context.fillRect(x, 0, canvasWidth - x, 20);
+          }
+        }
+      }
+    }
+  }, 1000);
+});
